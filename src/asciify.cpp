2e264d7aa7274797040fb999193f5a7212ca0f5d
Starfall <jinhaomin874@gmail.com>
2024-04-16 22:36:14 -0700

    Initial implementation of tokens

--- src/asciify.cpp
+++ src/asciify.cpp
@@ -13,3 +13,42 @@ void visualize(Image* img){
         printf("\n");
     }
 }
+
+
+// Token::Token(int* bits){
+//     for(int i = 0; i < len; i++){
+//         for(int j = 0; j < len; j++){
+//             int bit = bits[i*len+j];
+//             bitmap[i][j] = bit;
+//             lumin += bit;
+//         }
+//     }
+//     lumin = lumin >> 6; // divide by 64
+// }
+
+Token::Token(png_bytep* bytes, int token_size, int bit_len):
+    token_size{token_size}, bit_len{bit_len}
+{
+    bit_cnt = token_size / bit_len; // Length of bitmap row/col, %8=0 for bitmap comparison
+    for(int i = 0; i < token_size; i+=bit_len){
+        for(int j = 0; j < token_size; j+=bit_len){
+            // TODO
+            // 怎么将pixels压缩为bits?
+        }
+    }
+
+}
+
+TokenizedImage::TokenizedImage(Image* img, int token_size, int bit_len):
+    img{img}, token_size{token_size}, bit_len{bit_len}
+{
+    tokens_width = img->width/token_size+1;
+    tokens_height = img->height/token_size+1;
+    tokens = (Token**)malloc(sizeof(Token*)*tokens_height);
+    for(int y = 0; y < tokens_height; y++){
+        tokens[y] = (Token*)malloc(sizeof(Token)*tokens_width);
+        // TODO
+        // 分割img，新建token
+    }
+
+}
--- src/asciify.h
+++ src/asciify.h
@@ -3,5 +3,33 @@
 #include <string>
 using namespace std;
 
+// 计划：
+// Tokenize, 给定字符长度，将图片等分
+// 亮度匹配 (block) / 形状匹配 (XOR sum)
 // string asciify(Image* img);
 void visualize(Image* img);
+
+// 亮度：RGB*A/3
+// 先取average亮度
+class Token{
+    public:
+    const int token_size{}, bit_len{};
+    int bit_cnt, lumin;
+    int** bitmap;
+
+    Token(png_bytep* bytes, int token_size, int bit_len);
+};
+
+class TokenizedImage{
+    public:
+    Image* img{};
+    const int token_size{}, bit_len{};
+    int tokens_width, tokens_height;
+    Token **tokens;
+
+    TokenizedImage(Image* img, int token_size, int bit_len);
+};
+
+// Process:
+// 传入：img, token尺寸（最小8*8-形状 1*1亮度）
+// 如果不是8*8倍数不接受？
